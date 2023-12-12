36547ef988fcfce2236cf3487dd4d378e328ff9b
Jacob29 <armstrong.jake29@gmail.com>
2023-12-12 15:05:14 +0000

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,20 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings"""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_colour = (0, 0, 0)
+
+        # Ship settings
+        self.ship_speed = 4.6667
+
+        # Bullet settings
+        self.bullet_speed = 5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_colour = (60, 60, 60)
+        self.bullets_allowed = 10
+
